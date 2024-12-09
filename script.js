34ff47147d35717feb391578be2e134726a1ee79
vsbruna <141277861+vsbruna@users.noreply.github.com>
2024-12-09 11:29:13 -0300

    .

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementBy('formulario').addEventListener('submit', function (event){
+    event.preventDefault();
+
+    //Obter as notas inseridas pelo usuário
+    let nota1=parseFloat(document.getElementById('nota1').value);
+    let nota2=parseFloat(document.getElementById('nota2').value);
+    let nota3=parseFloat(document.getElementById('nota3').value);
+
+    //Calcular a média
+    let media=(nota1+nota2+nota3) /3;
+
+    //Exibir a mensagem do resultado
+    let resultadoDiv= document.getElementById('resultado')
+    if(media>=6.0){
+        resultadoDiv.innerHTML=
+
+}
